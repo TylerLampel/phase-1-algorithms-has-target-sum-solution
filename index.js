2f85680be82ed6f2fa227814f0b9d08d92c18063
Tyler Lampel <lampel13@gmail.com>
2022-05-30 09:19:48 -0400

    commit tree cf0933cc18395bcc12785a7cdae4792023e8767e

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = {}
+
+  for (const number of array) {
+    const complement = target - number
+    if (seenNumbers[complement]) return true
+    seenNumbers[number] = true
+  }
+
+  return false
 }
+
+
+// function hasTargetSum(array, target) {
+//   for (let  i = 0; i < array.length; i++) {
+//     // n steps
+//     const complement = target - array[i]
+//     for (let j = i + 1; j < array.length; j++){
+//       //n * n steps
+//       if(array[j] === complement) return true
+//     }
+//   }
+//   return false
+// }
 
 /* 
   Write the Big O time complexity of your function here
+  runtime: O(n*n)
+  space: O(n)
 */
 
 /* 
   Add your pseudocode here
+  create an object to keep track of the numbers weve already seen
+  hasTargetSum([22, 19, 4, 6, 30], 25)
+  iterate through each number in the array
+    for the current num, identify a complement that adds to the target (complement = target - num)
+    check if any key on our object is the complement
+      if so return true
+      otherwise add that number to the object
+
+  if i get to the end of the array, return false
 */
 
 /*
   Add written explanation of your solution here
+  make a function `hasTargetSum` that checks if 2 numbers from the array
+  add up to some target. for example, if i have an array `[1,2,3,4]`
+  and the target is `6` i should return true because 2 + 4 add up to 6.
+  for each nu,ber check if there's a number that adds up to the target.
 */
 
 // You can run `node index.js` to view these console logs
